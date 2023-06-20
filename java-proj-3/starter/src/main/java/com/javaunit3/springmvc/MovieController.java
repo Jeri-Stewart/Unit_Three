e30563e412510459405a543668d84c5761a45e53
Jeri Stewart <jerijuba@gmail.com>
2023-06-20 00:20:43 -0400

    Project 4 commit

--- java-proj-3/starter/src/main/java/com/javaunit3/springmvc/MovieController.java
+++ java-proj-3/starter/src/main/java/com/javaunit3/springmvc/MovieController.java
@@ -5,9 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.javaunit3.springmvc.model.MovieEntity;
+import com.javaunit3.springmvc.model.VoteEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 
 @Controller
 public class MovieController {
@@ -21,22 +26,81 @@ public class MovieController {
 
     @RequestMapping("/bestMovie")
         public String getBestMoviePage(Model model){
-            model.addAttribute("BestMovie", bestMovieService.getBestMovie().getTitle());
-            return "bestMovie";
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+
+        List<MovieEntity> movieEntityList = session.createQuery("from MovieEntity").list();
+        movieEntityList.sort(Comparator.comparing(movieEntity -> movieEntity.getVotes().size()));
+
+        MovieEntity movieWithMostVotes = movieEntityList.get(movieEntityList.size() - 1);
+        List<String> voterNames = new ArrayList<>();
+
+        for (VoteEntity vote: movieWithMostVotes.getVotes())
+        {
+            voterNames.add(vote.getVoterName());
+        }
+
+        String voterNamesList = String.join(",", voterNames);
+
+        model.addAttribute("bestMovie", movieWithMostVotes.getTitle());
+        model.addAttribute("bestMovieVoters", voterNamesList);
+
+        session.getTransaction().commit();
+
+        return "bestMovie";
         }
 
     @RequestMapping("/voteForBestMovieForm")
     public String voteForBestMovieFormPage(Model model) {
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        List<MovieEntity> movieEntityList = session.createQuery("from MovieEntity").list();
+        session.getTransaction().commit();
+        model.addAttribute("movies", movieEntityList);
         return "voteForBestMovie";
     }
     @RequestMapping("/voteForBestMovie")
     public String voteForBestMovie(HttpServletRequest request, Model model) {
-
-        String movieTitle = request.getParameter("movieTitle");
-
-        model.addAttribute("BestMovieVote", movieTitle);
+        String movieId = request.getParameter("movieId");
+        String voterName = request.getParameter("voterName");
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        MovieEntity movieEntity = (MovieEntity) session.get(MovieEntity.class, Integer.parseInt(movieId));
+        VoteEntity newVote = new VoteEntity();
+        newVote.setVoterName(voterName);
+        movieEntity.addVote(newVote);
+        session.update(movieEntity);
+        session.getTransaction().commit();
 
         return "voteForBestMovie";
+    }
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @RequestMapping("/addMovieForm")
+    public String addMovieForm() {
+        return "addMovie";
+    }
+
+    @RequestMapping("/addMovie")
+    public String addMovie(HttpServletRequest request) {
+        String movieTitle = request.getParameter("movieTitle");
+        String maturityRating = request.getParameter("maturityRating");
+        String genre = request.getParameter("genre");
+
+        MovieEntity movieEntity = new MovieEntity();
+        movieEntity.setTitle(movieTitle);
+        movieEntity.setMaturityRating(maturityRating);
+        movieEntity.setGenre(genre);
+
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        session.save(movieEntity);
+        session.getTransaction().commit();
+
+        return "addMovie";
+
     }
 
 
